ca4d5fb941aaf1412adc74a959e049ec560e3765
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-15 10:03:08 +0530

    adding two no's

---
+++ basicsOfCpp/add and sub (1).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+  int k,m,n,a,b,c,l;
+  cout<<"enter values of k,m,n,a,b,c,";
+  cin>>k>>m>>n>>a>>b>>c;
+  l=k+m+n-a-b-c;
+  cout<<"l="<<l;
+  getch();
+return 0;
+}
