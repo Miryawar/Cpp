9cbde3bbf8736de084f1d669b669f22f2ce9bb9c
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-15 10:11:59 +0530

    vovels and consonents

---
+++ basicsOfCpp/vovel and consonenet.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+
+int  main()        
+{
+  char n;
+  cout<<"enter n ";
+  cin>>n;
+   if (n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+  cout<<"vovel";
+  else
+  cout<<"consonent";
+  
+  getch();
+}
+    
