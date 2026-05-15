dfbe0835a521a1307c712e9cf004e145d3f23dc8
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-15 10:14:05 +0530

    last 2 digits of a no

---
+++ basicsOfCpp/last 2 digits of a number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main()
+{
+	int a,b;
+	cout<<"enter any number";
+	cin>>a;
+    b=a%100;
+	cout<<"last two digits of entered number are:"<<b;
+	return 0;
+	
+}
