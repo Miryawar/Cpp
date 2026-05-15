bccbf7c728975baf6889847786417b2ae143c47c
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-15 10:35:25 +0530

    linked list

---
+++ basicsOfCpp/linked_list.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedList {
+    Node* head;
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insert(int val) {
+        Node* newNode = new Node(val);
+        if (!head) head = newNode;
+        else {
+            Node* temp = head;
+            while (temp->next) temp = temp->next;
+            temp->next = newNode;
+        }
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+
+    ~LinkedList() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    LinkedList list;
+    int x,y,z;
+    cout << "enter data to be inserted" <<endl;
+    cin >> x>>y>>z;
+    list.insert(x);
+    list.insert(y);
+    list.insert(z);
+    list.display();
+    return 0;
+}
