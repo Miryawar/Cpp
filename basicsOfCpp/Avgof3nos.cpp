a82cad2a396be5d586afaf5f891866ad9f9702ab
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-15 10:04:12 +0530

    average of no's

---
+++ basicsOfCpp/Avgof3nos.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+
+int main()        
+{
+  int a,b,c,avg;
+  cout<<"enter values of a b and c:";
+  cin>>a>>b>>c;
+  avg=(a+b+c)/2;
+  cout<<"average of three nos is "<<avg;
+  
+  getch();
+  return 0;
+}
