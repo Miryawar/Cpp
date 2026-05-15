a1b8ffb93abc46a619ba2b7e0256a95fcb75b4bf
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-15 10:06:13 +0530

    circle area

---
+++ basicsOfCpp/area and cir of circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main()
+{
+ float r=7,A,C;
+ A=(3.14)*r*r;
+ C=2*3.14*r;
+ cout<<"area of circle is:"<<A;
+ cout<<".circumference of circle is:"<<C;
+ getch ();
+ return 0;
+}
